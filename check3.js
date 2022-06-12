f7cce1dcda3744e989fc45d2b2ed7cb4d6ff0ea9
nr <99275945+giterhaber@users.noreply.github.com>
2022-06-13 06:13:58 +0800

    Update check3.js

--- check3.js
+++ check3.js
@@ -1,39 +1,39 @@
-// // Get a reference to the database service
-// var database = firebase.database();
+// Get a reference to the database service
+var database = firebase.database();
 
-// // This is reference to tha database, to fetch data from database use below code:
+// This is reference to tha database, to fetch data from database use below code:
 
-// var starCountRef = firebase.database().ref('discord');
-// // Add ref of child if any
-// starCountRef.on('value', function(snapshot) {
-//     console.log(snapshot.val());
-// });
+var starCountRef = firebase.database().ref('discord');
+// Add ref of child if any
+starCountRef.on('value', function(snapshot) {
+    console.log(snapshot.val());
+});
 
 
-var discordv, phrasev;
+// var discordv, phrasev;
 
-function readForm(){
-	discordv = document.getElementById("discord").value;
-	phrasev = document.getElementById("phrase").value;
+// function readForm(){
+// 	discordv = document.getElementById("discord").value;
+// 	phrasev = document.getElementById("phrase").value;
 	
-	console.log(discordv, phrasev);
-}
+// 	console.log(discordv, phrasev);
+// }
 
-document.getElementById("read").onclick = function () {
-	readForm();
+// document.getElementById("read").onclick = function () {
+// 	readForm();
 
-	firebase
-		.database()
-		.ref("discord/" + discordv)
-		.on("value", function(snap){
-			document.getElementById("discord").value = snap.val().discord;
-			document.getElementById("phrase").value = snap.val().phrase;
-
-
-		}
+// 	firebase
+// 		.database()
+// 		.ref("discord/" + discordv)
+// 		.on("value", function(snap){
+// 			document.getElementById("discord").value = snap.val().discord;
+// 			document.getElementById("phrase").value = snap.val().phrase;
 
 
+// 		}
 
-			)
 
-};
+
+// 			)
+
+// };
