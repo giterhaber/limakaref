6cb49ccf548469039b249ba867f12a71d756e8d5
nr <99275945+giterhaber@users.noreply.github.com>
2022-06-13 05:28:46 +0800

    Create check3.js

---
+++ check3.js
@@ -0,0 +1,10 @@
+// Get a reference to the database service
+var database = firebase.database();
+
+// This is reference to tha database, to fetch data from database use below code:
+
+var starCountRef = firebase.database().ref('discord');
+// Add ref of child if any
+starCountRef.on('value', function(snapshot) {
+    console.log(snapshot.val());
+});
